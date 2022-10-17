823dc1dc7fa2992c28ce383e75be279835070508
teseus <teseus@gmail.com>
2022-10-17 16:42:31 +0900

    Change PowerLineSeparation.java

--- src/main/java/com/teseus/codetest/programmers/PowerLineSeparation.java
+++ src/main/java/com/teseus/codetest/programmers/PowerLineSeparation.java
@@ -4,39 +4,66 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
 public class PowerLineSeparation {
     class Solution {
         public int solution(int n, int[][] wires) {
-            List<Integer>[] nodes = getNodes(n);
-            putEdges(nodes, wires);
-            int ret = trace(nodes);
-            int answer = -1;
-            return answer;
+            if(n==0) {
+                return 0;
+            }
+            if(n==1) {
+                return wires[0][0] + wires[0][1];
+            }
+            List<Integer>[] edges;
+            int minDiffer = Integer.MAX_VALUE;
+            int minIdx = -1;
+            for (int i = 0; i < wires.length; i++) {
+                edges = clearEdges(n);
+                putEdges(edges, wires, i, i);
+                int sum1 = trace(0, edges);
+                edges = clearEdges(n);
+                putEdges(edges, wires, i, wires.length);
+                int sum2 = trace(i, edges);
+                if(minDiffer < Math.abs(sum1-sum2)){
+                    minDiffer = Math.abs(sum1-sum2);
+                    minIdx = i;
+                }
+            }
+            return minIdx;
         }
 
-        private int trace(List<Integer>[] nodes) {
+        private int trace(int start, List<Integer>[] nodes) {
             Stack<Integer> stack = new Stack<>();
-            stack.push(1);
+            stack.push(start);
+            boolean[] checks = new boolean[nodes.length];
+            Arrays.fill(checks, false);
+            int ans = 0;
             while(!stack.empty()){
                 Integer pop = stack.pop();
+                ans += pop;
+                checks[pop] = true;
                 for (int i = 0; i < nodes[pop].size(); i++) {
-                    stack.push(nodes[pop].get(i));
+                    Integer sub = nodes[pop].get(i);
+                    if(!checks[sub]) {
+                        stack.push(sub);
+                    }
                 }
                 System.out.println("pop = " + pop);
             }
-            return 0;
+            return ans;
         }
 
-        private void putEdges(List<Integer>[] nodes, int[][] wires) {
-            for (int i = 0; i < wires.length; i++) {
+        private void putEdges(List<Integer>[] nodes, int[][] wires, int s, int e) {
+            for (int i = s; i < e; i++) {
                 nodes[wires[i][0]].add(wires[i][1]);
+                nodes[wires[i][1]].add(wires[i][0]);
             }
         }
 
-        private List<Integer>[] getNodes(int num) {
+        private List<Integer>[] clearEdges(int num) {
             List<Integer>[] nodes = new List[num+1];
             for (int i = 1; i <= num; i++) {
                 nodes[i] = new ArrayList<>();
