18322b8a6c04862c178ae3fc85a7dda7b55a2387
teseus <teseus@gmail.com>
2022-10-18 13:05:44 +0900

    Add JadenCase 1st failed performance

---
+++ src/main/java/com/teseus/codetest/programmers/JadenCase.java
@@ -0,0 +1,39 @@
+package com.teseus.codetest.programmers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+// 처음은 생각 나는데로 작성했고,  동작은 제대로 했지만, 퍼포먼스가 낮아서 fail
+
+public class JadenCase {
+    class Solution {
+        public String solution(String s) {
+            String[] splits = s.split("\\s");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < splits.length; i++) {
+                if(splits[i].matches("^[a-z].*")) {
+                    sb.append((char) (splits[i].charAt(0) - 32));
+                } else {
+                    sb.append(splits[i].charAt(0));
+                }
+                sb.append(splits[i].substring(1).toLowerCase());
+                if(i < splits.length-1){
+                    sb.append(" ");
+                }
+            }
+            return sb.toString();
+        }
+    }
+
+    @Test
+    public void test1(){
+        //when
+        String result1 = new Solution().solution("for thE lasT weeK");
+        //then
+        Assert.assertEquals("For The Last Week", result1);
+        //when
+        String result = new Solution().solution("3people unFollowed me");
+        //then
+        Assert.assertEquals("3people Unfollowed Me", result);
+    }
+}
