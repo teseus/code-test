fdbde2e1971a5b5b5c2860927e6eea85e17b92c7
teseus <teseus@gmail.com>
2022-10-21 21:22:18 +0900

    Add JoyStick 2st not judge

--- src/main/java/com/teseus/codetest/programmers/JoyStick.java
+++ src/main/java/com/teseus/codetest/programmers/JoyStick.java
@@ -8,8 +8,8 @@ public class JoyStick {
         int all = ('Z' - 'A') + 1;
         int mid = all /2;
 
-        boolean lastA = false;
-        int contA = 0;
+        int lastA = -1;
+        int lenA = 0;
 
         public int solution(String name) {
             int distanceSum = 0;
@@ -21,56 +21,58 @@ public class JoyStick {
                     distanceSum += (all-dist);
                 }
 
-
                 if(name.charAt(i) == 'A') {
-                    if(lastA) {
-
+                    if(lastA != -1) {
+                        lastA = i;
                     }
-                    lastA = true;
+                    lenA = ( i - lastA ) + 1;
+                } else {
+                    lastA = -1;
                 }
             }
+
+            if(name.charAt(name.length()-1) == 'A') { //마지막이 A군
+                return distanceSum + lastA-1;
+            } else if (lastA == 0) { //처음이 A군
+                return distanceSum + name.length() - lenA;
+            } else if (lenA > 0) {
+                int notAlen = lastA;
+                if(notAlen > lenA) {
+                    return distanceSum + (name.length()-1);
+                } else {
+                    return distanceSum + notAlen;
+                }
+            }
+            //BBABABAAAAAAC
             //뒤가 A 이면 아닌것들만 더한다 -1 .  BAAA = 0  BBBAA = 3-1 = 2
             //앞이 A 이면 아닌것 갯수만큼 더한다. AAAB = 1, AAABBB  = 3
             //중간에 A 가 있다면 연속한 A 중에 가장 긴것이 전체 개수보다 같거나 크다면 전체 - 1 BBAAAC = 5
+            // BBAAAC = min(5, 3) back, BBBAAAC = min(6,5) back, BBBBAAAC = (7,7) back, forward,
+            // BBBBBAAAC = min(8,9) 8 forward A군 앞에것의 갯수가 A군보다 클때만 포워드 방향이다.
+
+            //ABBBBAAAAC 이런경우는 어떻게 되는가?
+            //AAAAAAAA 이면?
             return distanceSum + (name.length()-1);
         }
     }
 
-//중간에 A ㄱ
-
-
-//012345 6789 짝수
-//10/2 = 5
-//5-0 <= 5
-//6-0 > 5 -> 10-6 = 4
-
-//01234 5678 홀수
-//9/2 = 4
-//4-0 <= 4
-//5-0 > 4 -> 9-5 = 4
-
-//A가 사이사이 섞인 경우는?
-//    A로 끝나는 경우? - 마지막에서 A를 다 뺀다.
-//    A로 시작하는 경우
-//    중간에 A 인경우는 뒤에 있는 A 의 갯수와 같거나 작으면 돌아간다.
-
     @Test
     public void test1(){
         //when
-        int result3 = new Solution().solution("JAAAAAAAN");
+        int result3 = new Solution().solution("BAAAAAAAC");
         //then
-        Assert.assertEquals(23, result3);
-        //when
-        int result2 = new Solution().solution("JAAAAAAAN");
-        //then
-        Assert.assertEquals(23, result2);
-        //when
-        int result1 = new Solution().solution("JAAAAAAAN");
-        //then
-        Assert.assertEquals(23, result1);
-        //when
-        int result = new Solution().solution("JEROEN");
-        //then
-        Assert.assertEquals(56, result);
+        Assert.assertEquals(4, result3);
+//        //when
+//        int result2 = new Solution().solution("JAAAAAAAN");
+//        //then
+//        Assert.assertEquals(23, result2);
+//        //when
+//        int result1 = new Solution().solution("JAAAAAAAN");
+//        //then
+//        Assert.assertEquals(23, result1);
+//        //when
+//        int result = new Solution().solution("JEROEN");
+//        //then
+//        Assert.assertEquals(56, result);
     }
 }
