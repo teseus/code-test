70598264fb33413dc9ab22a3c844771cfd9e562e
teseus <teseus@gmail.com>
2022-11-08 07:43:08 +0900

    Add Intersection 2nd succeeds

--- src/main/java/com/teseus/codetest/inflearn/Intersection.java
+++ src/main/java/com/teseus/codetest/inflearn/Intersection.java
@@ -4,23 +4,31 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 // 처음 생각 나는 데로 했으나, 시간 초과 에러 발생
+// 두개의 배열을 먼저 소팅하고, 소팅한 결과들로 루프를 한번 돌게 하는 로직으로 변경해서 성공, 답을 참고 했다.
 
 public class Intersection {
     static class Solution {
-        public int[] solution(int[] arr1, int[] arr2) {
+        public List<Integer> solution(int[] arr1, int[] arr2) {
             List<Integer> inter = new ArrayList<>();
-            for (int i = 0; i < arr1.length; i++) {
-                for (int j = 0; j < arr2.length; j++) {
-                    if(arr1[i] ==  arr2[j]) {
-                        inter.add(arr1[i]);
-                    }
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            int a=0, b=0;
+            while (a<arr1.length && b<arr2.length) {
+                if(arr1[a] == arr2[b]){
+                    inter.add(arr1[a++]);
+                    b++;
+                } else if (arr1[a] < arr2[b]) {
+                    a++;
+                } else {
+                    b++;
                 }
             }
-            return inter.stream().sorted(Integer::compareTo).mapToInt(Integer::intValue).toArray();
+            return inter;
         }
     }
 
@@ -37,8 +45,8 @@ public class Intersection {
             nums2[i] = in.nextInt();
         }
 
-        int[] ans = new Solution().solution(nums1, nums2);
-        for (int an : ans) {
+        List<Integer> arns = new Solution().solution(nums1, nums2);
+        for (int an : arns) {
             System.out.print(an+" ");
         }
     }
@@ -47,8 +55,8 @@ public class Intersection {
     @Test
     public void test1(){
         //when
-        int[] result = new Solution().solution(new int[] {1, 3, 9, 5, 2}, new int[] {3, 2, 5, 7, 8});
+        List<Integer> result = new Solution().solution(new int[] {1, 3, 9, 5, 2}, new int[] {3, 2, 5, 7, 8});
         //then
-        Assert.assertArrayEquals(new int[] {2, 3, 5}, result);
+        Assert.assertEquals(Arrays.asList(2,3,5), result);
     }
 }
