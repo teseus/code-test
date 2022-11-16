44ce52cf74a1f842e1ca85b0f751e095f316809f
teseus <teseus@gmail.com>
2022-11-16 10:12:27 +0900

    Change inflearn template to check output stream

--- src/main/java/com/teseus/codetest/inflearn/Template.java
+++ src/main/java/com/teseus/codetest/inflearn/Template.java
@@ -1,11 +1,22 @@
 package com.teseus.codetest.inflearn;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
+import static org.junit.Assert.assertEquals;
+
 public class Template {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
     static class Solution {
         public int solution(int[] n) {
             return 0;
@@ -26,5 +37,29 @@ public class Template {
         int result = new Solution().solution(new int[]{1,3});
         //then
         Assert.assertEquals(2, result);
+    }
+
+    @Test
+    public void out() {
+        System.out.print("hello");
+        assertEquals("hello", outContent.toString());
+    }
+
+    @Test
+    public void err() {
+        System.err.print("hello again");
+        assertEquals("hello again", errContent.toString());
+    }
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
     }
 }
