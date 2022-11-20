b63570f3f21ea8fbde7b0af3dd16e8876c327fcb
teseus <teseus@gmail.com>
2022-11-21 00:07:09 +0900

    Second try

--- src/main/java/com/teseus/codetest/programmers/ohouse/TestOne.java
+++ src/main/java/com/teseus/codetest/programmers/ohouse/TestOne.java
@@ -3,10 +3,15 @@ package com.teseus.codetest.programmers.ohouse;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 
 public class TestOne {
         class Solution {
-            class Room {
+            class Room implements Comparable<Room>{
                 int idx, until, count;
 
                 public Room(int idx, int until, int count) {
@@ -14,51 +19,54 @@ public class TestOne {
                     this.until = until;
                     this.count = count;
                 }
+
+                @Override
+                public int compareTo(Room o) {
+                    if(this.until == o.until) {
+                        return this.idx - o.idx;
+                    }
+                    return this.until - o.until;
+                }
             }
 
             public int solution(int n, int[][] meetings) {
-                Room[] rooms = new Room[n];
-                int earlyIdx = Integer.MAX_VALUE;
-                int earlyTime = Integer.MAX_VALUE;
-                int maxIdx = 0;
-                int maxCount = 1;
-                for (int i = 0; i < n && i < meetings.length; i++) {
-                    rooms[i] = new Room(i, meetings[i][1], 1);
-                    if(earlyTime > rooms[i].until) {
-                        earlyIdx = i;
-                        earlyTime = rooms[i].until;
-                    }
-                    System.out.println("meeting no:" + i + ", until:" + rooms[i].until + ", count:" + rooms[i].count);
-                }
+                PriorityQueue<Room> earlyRoom = new PriorityQueue<>();
+                List<Room> list = new ArrayList<>();
 
-                System.out.println("earlyIdx= " + earlyIdx + ", earlyTime= " + earlyTime);
+                for (int i = 0; i < n && i < meetings.length; i++) {
+                    Room r = new Room(i, meetings[i][1], 1);
+                    earlyRoom.add(r);
+                    list.add(r);
+                }
 
                 for (int i = n; i < meetings.length; i++) {
-                    rooms[earlyIdx].until += (meetings[i][1] - meetings[i][0]);
-                    rooms[earlyIdx].count++;
-                    System.out.println("input room no:" + earlyIdx + ", until:" + rooms[earlyIdx].until + ", count:" + rooms[earlyIdx].count);
-                    earlyTime = Integer.MAX_VALUE;
-                    maxCount = 0;
-                    for (int j = 0; j < n; j++) {
-                        if(earlyTime > rooms[j].until) {
-                            earlyIdx = j;
-                            earlyTime = rooms[j].until;
-                            System.out.println("after earlyIdx= " + earlyIdx + ", until = " + rooms[earlyIdx].until);
-                        }
-                        if(maxCount<rooms[j].count) {
-                            maxIdx = j;
-                            maxCount = rooms[j].count;
-                            System.out.println("after maxIdx= " + maxIdx + ", maxCount= " + maxCount);
-                        }
-                    }
+                    Room earliestRoom = earlyRoom.poll();
+                    System.out.println("earliest room no:" + earliestRoom.idx + ", until:" + earliestRoom.until + ", count:" + earliestRoom.count);
+                    earliestRoom.until += (meetings[i][1] - meetings[i][0]);
+                    earliestRoom.count ++;
+                    System.out.println("input room no:" + earliestRoom.idx + ", until:" + earliestRoom.until + ", count:" + earliestRoom.count);
+                    earlyRoom.add(earliestRoom);
                 }
 
-                return maxIdx;
+                return list.stream().max(Comparator.comparing(it->it.count)).get().idx;
             }
         }
 
     @Test
     public void test1(){
+        //when
+        int result4 = new Solution().solution(100, new int[][]{{((5*10^5)-1),(5*10^5)}});
+        //then
+        Assert.assertEquals(0, result4);
+        //when
+        int result2 = new Solution().solution(1, new int[][]{{1,4}, {2,5}, {4,10}, {5,7}, {7,9}});
+        //then
+        Assert.assertEquals(0, result2);
+
+        //when
+        int result1 = new Solution().solution(2, new int[][]{{1,4}, {2,5}, {4,10}, {5,7}, {7,9}});
+        //then
+        Assert.assertEquals(1, result1);
         //when
         int result = new Solution().solution(3, new int[][]{{1,20},{2,10},{3,5},{4,9},{6,8}});
         //then
