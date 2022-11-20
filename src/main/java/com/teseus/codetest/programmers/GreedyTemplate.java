7812e38fe24f71f0e43daeef9498e445aaadba15
teseus <teseus@gmail.com>
2022-11-20 18:06:02 +0900

    Add Solution in GreedyTemplate

--- src/main/java/com/teseus/codetest/programmers/GreedyTemplate.java
+++ src/main/java/com/teseus/codetest/programmers/GreedyTemplate.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class GreedyTemplate {
@@ -20,12 +21,20 @@ public class GreedyTemplate {
             if(o.end == this.end) {
                 return this.start - o.end;
             }
-            return this.end - this.start;
+            return this.end - o.end;
         }
     }
     class Solution {
         public int solution(List<Time> arr, int num) {
             int cnt = 0;
+            Collections.sort(arr);
+            int endTime=0;
+            for (Time time : arr) {
+                if(time.start>=endTime) {
+                    cnt++;
+                    endTime = time.end;
+                }
+            }
             return cnt;
         }
     }
@@ -38,5 +47,11 @@ public class GreedyTemplate {
         Arrays.asList(new Time(3,3),new Time(2,3),new Time(1,3));
         int result = solution.solution(Arrays.asList(new Time(3,3),new Time(2,3),new Time(1,3)), 3);
         Assert.assertEquals(2, result);
+        //given
+        Solution solution1 = new Solution();
+        //when
+        List<Time> times1 = Arrays.asList(new Time(1, 4), new Time(2, 3), new Time(3, 5), new Time(4, 6), new Time(5, 7));
+        int result1 = solution1.solution(times1, 5);
+        Assert.assertEquals(3, result1);
     }
 }
